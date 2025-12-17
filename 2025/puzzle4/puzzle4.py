4f00de7a17e9c090c0e9b31024292879e2873da5
David Bell[C <david.bell@gmail.com>
2025-12-17 20:02:59 +0000

    puzzle 4 part 2

--- 2025/puzzle4/puzzle4.py
+++ 2025/puzzle4/puzzle4.py
@@ -11,3 +11,23 @@ for line in lines:
     x, y = tx, ty
 
 print(distance)
+
+distance = 0
+x, y = 0, 0
+for line in lines:
+    tx, ty = [int(x) for x in line.split(",")]
+
+    x_dist = abs(tx - x)
+    y_dist = abs(ty - y)
+
+    if x_dist == y_dist:
+        distance += x_dist
+        x_dist, y_dist = 0, 0
+    elif x_dist < y_dist:
+        distance += x_dist + (y_dist - x_dist)
+    elif y_dist < x_dist:
+        distance += y_dist + (x_dist - y_dist)
+
+    x, y = tx, ty
+
+print(distance)
