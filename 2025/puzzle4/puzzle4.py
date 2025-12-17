c7d1cde5403d883867f64ef81f6ea9d3fb83fff8
David Bell[C <david.bell@gmail.com>
2025-12-17 20:13:56 +0000

    puzzle 4 part 3

--- 2025/puzzle4/puzzle4.py
+++ 2025/puzzle4/puzzle4.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 
+def diag_distance(trash):
+    distance = 0
+    x, y = 0, 0
+    for tx, ty in trash:
+        x_dist = abs(tx - x)
+        y_dist = abs(ty - y)
+
+        if x_dist == y_dist:
+            distance += x_dist
+        elif x_dist < y_dist:
+            distance += x_dist + (y_dist - x_dist)
+        elif y_dist < x_dist:
+            distance += y_dist + (x_dist - y_dist)
+
+        x, y = tx, ty
+    return distance
+
 f = open('input.txt')
 lines = f.read().splitlines()
 
+trash = []
+for line in lines:
+    x, y = [int(n) for n in line.split(",")]
+    trash.append((x, y))
+
 distance = 0
 x, y = 0, 0
-for line in lines:
-    tx, ty = [int(x) for x in line.split(",")]
+for tx, ty in trash:
     distance += abs(tx - x) + abs(ty - y)
     x, y = tx, ty
-
 print(distance)
 
-distance = 0
-x, y = 0, 0
-for line in lines:
-    tx, ty = [int(x) for x in line.split(",")]
-
-    x_dist = abs(tx - x)
-    y_dist = abs(ty - y)
-
-    if x_dist == y_dist:
-        distance += x_dist
-        x_dist, y_dist = 0, 0
-    elif x_dist < y_dist:
-        distance += x_dist + (y_dist - x_dist)
-    elif y_dist < x_dist:
-        distance += y_dist + (x_dist - y_dist)
-
-    x, y = tx, ty
-
-print(distance)
+print(diag_distance(trash))
+trash = sorted(trash, key=lambda n : n[0] + n[1])
+print(diag_distance(trash))
