fa845d526f51e03aeab173c889ba2c92f2f9b73b
David Bell[C <david.bell@gmail.com>
2025-12-18 18:53:29 +0000

    refactor

--- 2025/puzzle6/puzzle6.py
+++ 2025/puzzle6/puzzle6.py
@@ -7,66 +7,33 @@ class Bird:
         self.vx = vx
         self.vy = vy
 
-f = open('input.txt')
-#f = open('test.txt')
-lines = f.read().splitlines()
+def take_pictures(birds, seconds, repeats):
+    for bird in birds:
+        bird.x = 0
+        bird.y = 0
+
+    count = 0
+    for _ in range(repeats):
+        for bird in birds:
+            bird.x = (bird.x + bird.vx * seconds) % size
+            bird.y = (bird.y + bird.vy * seconds) % size
+
+            if start <= bird.x < end and start <= bird.y < end:
+                count += 1
+    return count
 
 size = 1000
-#size = 8
-seconds = 100
 start = size // 4
 end = start * 3
 
-print(size, start, end)
+f = open('input.txt')
+lines = f.read().splitlines()
 
 birds = []
 for line in lines:
     vx, vy = line.split(",")
     birds.append(Bird(0, 0, int(vx), int(vy)))
 
-for bird in birds:
-    bird.x = (bird.x + bird.vx * seconds) % size
-    bird.y = (bird.y + bird.vy * seconds) % size
-        
-count = 0
-for bird in birds:
-    if start <= bird.x < end and start <= bird.y < end:
-        count += 1
-
-print(count)
-
-
-birds = []
-for line in lines:
-    vx, vy = line.split(",")
-    birds.append(Bird(0, 0, int(vx), int(vy)))
-
-count = 0
-for _ in range(1000):
-    for bird in birds:
-        bird.x = (bird.x + bird.vx * 3600) % size
-        bird.y = (bird.y + bird.vy * 3600) % size
-
-    for bird in birds:
-        if start <= bird.x < end and start <= bird.y < end:
-            count += 1
-
-print(count)
-
-
-birds = []
-for line in lines:
-    vx, vy = line.split(",")
-    birds.append(Bird(0, 0, int(vx), int(vy)))
-
-count = 0
-for _ in range(1000):
-    for bird in birds:
-        bird.x = (bird.x + bird.vx * 31556926) % size
-        bird.y = (bird.y + bird.vy * 31556926) % size
-
-    for bird in birds:
-        if start <= bird.x < end and start <= bird.y < end:
-            count += 1
-
-print(count)
+print(take_pictures(birds, 100, 1))
+print(take_pictures(birds, 3600, 1000))
+print(take_pictures(birds, 31556926, 1000))
