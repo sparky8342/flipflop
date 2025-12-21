92a296156e198324d96069513c0bb20746ecd619
David Bell[C <david.bell@gmail.com>
2025-12-21 18:54:19 +0000

    more refactor

--- 2025/puzzle7/puzzle7.py
+++ 2025/puzzle7/puzzle7.py
@@ -6,15 +6,7 @@ f = open('input.txt')
 lines = f.read().splitlines()
 
 @functools.cache
-def path_count(x, y):
-    if x < 0 or y < 0:
-        return 0
-    if x == 0 and y == 0:
-        return 1
-    return path_count(x-1, y) + path_count(x, y-1)
-
-@functools.cache
-def path_count_multi(dimensions):
+def path_count(dimensions):
     if any(val < 0 for val in dimensions):
         return 0
     if all(val == 0 for val in dimensions):
@@ -24,20 +16,22 @@ def path_count_multi(dimensions):
     for i in range(len(dimensions)):
         d_copy = list(dimensions)
         d_copy[i] -= 1
-        total += path_count_multi(tuple(d_copy))
+        total += path_count(tuple(d_copy))
     return total
 
 part1, part2, part3 = 0, 0, 0
 for line in lines:
     x, y = [int(n) for n in line.split()]
-    part1 += path_count(x-1, y-1)
+
+    dimensions = (x-1, y-1)
+    part1 += path_count(dimensions)
 
     dimensions = (x-1, y-1, x-1)
-    part2 += path_count_multi(dimensions)
+    part2 += path_count(dimensions)
 
     dimensions = []
     for _ in range(x):
         dimensions.append(y-1)
-    part3 += path_count_multi(tuple(dimensions))
+    part3 += path_count(tuple(dimensions))
 
 print(f"{part1}\n{part2}\n{part3}")
