9645a90fa6d5bc81b11de8251cf1b43e1858bddf
David Bell[C <david.bell@gmail.com>
2025-12-21 18:43:32 +0000

    puzzle 7 part 3

--- 2025/puzzle7/puzzle7.py
+++ 2025/puzzle7/puzzle7.py
@@ -1,76 +1,57 @@
 #!/usr/bin/python3
+import copy
+import functools
 
 f = open('input.txt')
-#f = open('test.txt')
 lines = f.read().splitlines()
 
-def paths(width, height):
-    grid = []
-    for _ in range(height):
-        row = []
-        for _ in range(width):
-            row.append(0)
-        grid.append(row)
+@functools.cache
+def path_count(x, y):
+    if x < 0 or y < 0:
+        return 0
+    if x == 0 and y == 0:
+        return 1
+    return path_count(x-1, y) + path_count(x, y-1)
 
-    for y in range(height):
-        grid[y][0] = 1
+@functools.cache
+def path_count3d(x, y, z):
+    if x < 0 or y < 0 or z < 0:
+        return 0
+    if x == 0 and y == 0 and z == 0:
+        return 1
+    return path_count3d(x-1, y, z) + path_count3d(x, y-1, z) + path_count3d(x, y, z-1)
 
-    for x in range(width):
-        grid[0][x] = 1
+@functools.cache
+def path_count_multi(dimensions):
+    if any(val < 0 for val in dimensions):
+        return 0
+    if all(val == 0 for val in dimensions):
+        return 1
 
-    for y in range(1, height):
-        for x in range(1, width):
-            grid[y][x] = grid[y-1][x] + grid[y][x-1]
-
-    return grid[height-1][width-1]
-
-def paths3d(width, height, depth):
-    grid = []
-    for _ in range(height):
-        row = []
-        for _ in range(width):
-            line = []
-            for _ in range(depth):
-                line.append(0)
-            row.append(line)
-        grid.append(row)
-
-    for y in range(height):
-        grid[y][0][0] = 1
-
-    for x in range(width):
-        grid[0][x][0] = 1
-
-    for z in range(depth):
-        grid[0][0][z] = 1
-
-    for y in range(1, height):
-        for x in range(1, width):
-            grid[y][x][0] = grid[y-1][x][0] + grid[y][x-1][0]
-
-    for y in range(1, height):
-        for z in range(1, depth):
-            grid[y][0][z] = grid[y-1][0][z] + grid[y][0][z-1]
-
-    for x in range(1, width):
-        for z in range(1, depth):
-            grid[0][x][z] = grid[0][x-1][z] + grid[0][x][z-1]
-
-    for y in range(1, height):
-        for x in range(1, width):
-            for z in range(1, depth): 
-                grid[y][x][z] = grid[y-1][x][z] + grid[y][x-1][z] + grid[y][x][z-1]
-
-    return grid[height-1][width-1][depth-1]
+    total = 0
+    for i in range(len(dimensions)):
+        d_copy = list(dimensions)
+        d_copy[i] -= 1
+        total += path_count_multi(tuple(d_copy))
+    return total
 
 total = 0
 for line in lines:
     width, height = line.split()
-    total += paths(int(width), int(height))
+    total += path_count(int(width)-1, int(height)-1)
 print(total)
 
 total = 0
 for line in lines:
     width, height = line.split()
-    total += paths3d(int(width), int(height), int(width))
+    total += path_count3d(int(width)-1, int(height)-1, int(width)-1)
+print(total)
+
+total = 0
+for line in lines:
+    amount, size = line.split()
+    dimensions = []
+    for _ in range(0, int(amount)):
+        dimensions.append(int(size)-1)
+    total += path_count_multi(tuple(dimensions))
 print(total)
