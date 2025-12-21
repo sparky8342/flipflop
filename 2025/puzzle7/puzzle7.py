86383d054203d5de89726a73be28fc068c1d761e
David Bell[C <david.bell@gmail.com>
2025-12-21 18:52:04 +0000

    refactor

--- 2025/puzzle7/puzzle7.py
+++ 2025/puzzle7/puzzle7.py
@@ -14,14 +14,6 @@ def path_count(x, y):
     return path_count(x-1, y) + path_count(x, y-1)
 
 @functools.cache
-def path_count3d(x, y, z):
-    if x < 0 or y < 0 or z < 0:
-        return 0
-    if x == 0 and y == 0 and z == 0:
-        return 1
-    return path_count3d(x-1, y, z) + path_count3d(x, y-1, z) + path_count3d(x, y, z-1)
-
-@functools.cache
 def path_count_multi(dimensions):
     if any(val < 0 for val in dimensions):
         return 0
@@ -35,23 +27,17 @@ def path_count_multi(dimensions):
         total += path_count_multi(tuple(d_copy))
     return total
 
-total = 0
+part1, part2, part3 = 0, 0, 0
 for line in lines:
-    width, height = line.split()
-    total += path_count(int(width)-1, int(height)-1)
-print(total)
+    x, y = [int(n) for n in line.split()]
+    part1 += path_count(x-1, y-1)
 
-total = 0
-for line in lines:
-    width, height = line.split()
-    total += path_count3d(int(width)-1, int(height)-1, int(width)-1)
-print(total)
+    dimensions = (x-1, y-1, x-1)
+    part2 += path_count_multi(dimensions)
 
-total = 0
-for line in lines:
-    amount, size = line.split()
     dimensions = []
-    for _ in range(0, int(amount)):
-        dimensions.append(int(size)-1)
-    total += path_count_multi(tuple(dimensions))
-print(total)
+    for _ in range(x):
+        dimensions.append(y-1)
+    part3 += path_count_multi(tuple(dimensions))
+
+print(f"{part1}\n{part2}\n{part3}")
