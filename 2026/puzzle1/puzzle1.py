2b06ea998ed6263414eb13ee9ab4f654c7e7b2d8
David Bell[C <david.bell@gmail.com>
2026-07-06 12:51:28 +0100

    2026 puzzle 1 part 3

--- 2026/puzzle1/puzzle1.py
+++ 2026/puzzle1/puzzle1.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 
-p1, p2 = 0, 0
+part1, part2 = 0, 0
 
-f = open('input.txt')
-for line in f.read().splitlines():
-    t = int(line)
-    if t < 60:
-        p1 += 60 - t
-    elif t > 60:
-        p2 += (t - 60) * 5
+temps = [int(x) for x in open('input.txt').read().splitlines()]
 
-p2 += p1
+for temp in temps:
+    if temp < 60:
+        part1 += 60 - temp
+    elif temp > 60:
+        part2 += (temp - 60) * 5
 
-print(p1, p2)
+part2 += part1
+
+part3 = 0
+amount = len(temps) // 2
+for i in range(0, amount):
+    temp = temps[i]
+    desired = temps[i+amount]
+    if desired < temp:
+        part3 += (temp - desired) * 5
+    elif desired > temp:
+        part3 += desired - temp
+
+print(f"{part1}\n{part2}\n{part3}")
