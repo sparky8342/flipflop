47a14962236fbfa38f7b3a17c31b2d2d689c627e
David Bell[C <david.bell@gmail.com>
2026-07-07 14:18:16 +0100

    2026 puzzle 2 part 2

--- 2026/puzzle2/puzzle2.py
+++ 2026/puzzle2/puzzle2.py
@@ -5,22 +5,45 @@ WALL_SIZE = 100
 moves = list(open('input.txt').read().strip())
 
 wall = [0] * WALL_SIZE
-pos = 0
+robot = 0
 
 for move in moves:
     if move == '<':
-        pos -= 1
+        robot -= 1
     elif move == '>':
-        pos += 1
-    pos %= WALL_SIZE
-    wall[pos] += 1
+        robot += 1
+    robot %= WALL_SIZE
+    wall[robot] += 1
 
 max_heat = wall[0]
-max_pos = 0
+max_robot = 0
 
 for i, heat in enumerate(wall):
     if heat > max_heat:
         max_heat = heat
-        max_pos = i
+        max_robot = i
 
-print((max_pos + 1) * max_heat)
+part1 = (max_robot + 1) * max_heat
+
+robot = 0
+wall_segment = 0
+part2 = 0
+rev_moves = list(reversed(moves))
+
+for i in range(0, len(moves)):
+    if moves[i] == '<':
+        robot -= 1
+    elif moves[i] == '>':
+        robot += 1
+    robot %= WALL_SIZE
+
+    if rev_moves[i] == '<':
+        wall_segment -= 1
+    elif rev_moves[i] == '>':
+        wall_segment += 1
+    wall_segment %= WALL_SIZE
+
+    if robot == wall_segment:
+       part2 += 1
+
+print(f"{part1}\n{part2}")
