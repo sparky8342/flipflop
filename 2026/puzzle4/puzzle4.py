d077bca2f9a7d86669e4ea68de17bb583af93360
David Bell[C <david.bell@gmail.com>
2026-07-09 17:30:29 +0100

    2026 puzzle 4 part 2

--- 2026/puzzle4/puzzle4.py
+++ 2026/puzzle4/puzzle4.py
@@ -10,3 +10,22 @@ for i in range(cut, 3, -1):
         leaves += 1
 
 print(leaves)
+
+swaps = 0
+last_side = ""
+for i in range(len(flower) - 2, 2, -1):
+    side = ""
+    if flower[i][0] == 'o':
+        side = "L"
+    elif len(flower[i]) == 5 and flower[i][4] == 'o':
+        side = "R"
+    else:
+        continue
+
+    if last_side == "":
+        last_side = side
+    elif last_side != side:
+        swaps += 1
+        last_side = side
+
+print(swaps)
