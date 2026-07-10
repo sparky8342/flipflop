caeb2eae58543ee2729a3c06415c13ce19a90d2a
David Bell[C <david.bell@gmail.com>
2026-07-10 13:06:04 +0100

    2026 puzzle 5 part 3

--- 2026/puzzle5/puzzle5.py
+++ 2026/puzzle5/puzzle5.py
@@ -22,11 +22,52 @@ def distinct_streets(grid):
         visited.add(loc)
 
 
+def distinct_streets_with_turns(grid):
+    turns = 0
+    x, y = 0, 0
+    visited = set()
+    visited.add((0, 0))
+
+    illegal_turn = False
+    while True:
+        if illegal_turn:
+            match grid[y][x]:
+                case 'v':
+                    x -= 1
+                case '<':
+                    y -= 1
+                case '^':
+                    x += 1
+                case '>':
+                    y += 1
+            illegal_turn = False
+        else:
+            match grid[y][x]:
+                case 'v':
+                    y += 1
+                case '<':
+                    x -= 1
+                case '^':
+                    y -= 1
+                case '>':
+                    x += 1
+
+        loc = (x, y)
+        if loc in visited:
+            if turns == 3 or y == 0 or y == size - 1 or x == 0 or x == size - 1:
+                return len(visited)
+
+            illegal_turn = True
+            turns += 1
+            continue
+        visited.add(loc)
+
+
 grid = []
 for line in open('input.txt').read().splitlines():
     grid.append(list(line))
 
-print(distinct_streets(grid))
+part1 = distinct_streets(grid)
 
 size = len(grid)
 max_amount = 0
@@ -40,9 +81,26 @@ for y in range(1, size - 1):
                 continue
             grid[y][x] = direction
             amount = distinct_streets(grid)
-            if amount > max_amount:
-                max_amount = amount
+            max_amount = max(amount, max_amount)
 
         grid[y][x] = val
 
-print(max_amount)
+part2 = max_amount
+
+max_amount = 0
+for y in range(1, size - 1):
+    for x in range(1, size - 1):
+
+        val = grid[y][x]
+        for direction in ['v', '<', '^', '>']:
+            if val == direction:
+                continue
+            grid[y][x] = direction
+            amount = distinct_streets_with_turns(grid)
+            max_amount = max(amount, max_amount)
+
+        grid[y][x] = val
+
+part3 = max_amount
+
+print(f"{part1}\n{part2}\n{part3}")
