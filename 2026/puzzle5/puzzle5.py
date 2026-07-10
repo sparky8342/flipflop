69f9b1433b7bb163fc963f7a8aed392349eed3d8
David Bell[C <david.bell@gmail.com>
2026-07-10 12:08:00 +0100

    2026 puzzle 5 part 2

--- 2026/puzzle5/puzzle5.py
+++ 2026/puzzle5/puzzle5.py
@@ -1,25 +1,48 @@
 #!/usr/bin/python3
 
-grid = open('input.txt').read().splitlines()
+def distinct_streets(grid):
+    x, y = 0, 0
+    visited = set()
+    visited.add((0, 0))
 
-x, y = 0, 0
-visited = set()
-visited.add((0, 0))
+    while True:
+        match grid[y][x]:
+            case 'v':
+                y += 1
+            case '<':
+                x -= 1
+            case '^':
+                y -= 1
+            case '>':
+                x += 1
 
-while True:
-    match grid[y][x]:
-        case 'v':
-            y += 1
-        case '<':
-            x -= 1
-        case '^':
-            y -= 1
-        case '>':
-            x += 1
+        loc = (x, y)
+        if loc in visited:
+            return len(visited)
+        visited.add(loc)
 
-    loc = (x, y)
-    if loc in visited:
-        break
-    visited.add(loc)
 
-print(len(visited))
+grid = []
+for line in open('input.txt').read().splitlines():
+    grid.append(list(line))
+
+print(distinct_streets(grid))
+
+size = len(grid)
+max_amount = 0
+
+for y in range(1, size - 1):
+    for x in range(1, size - 1):
+
+        val = grid[y][x]
+        for direction in ['v', '<', '^', '>']:
+            if val == direction:
+                continue
+            grid[y][x] = direction
+            amount = distinct_streets(grid)
+            if amount > max_amount:
+                max_amount = amount
+
+        grid[y][x] = val
+
+print(max_amount)
