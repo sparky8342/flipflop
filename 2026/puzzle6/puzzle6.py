dc1693da4e21e4e1ca92f627df1cdb701eb71198
David Bell <david.bell@gmail.com>
2026-07-12 12:16:02 +0100

    create Machine class to encapsulate logic

--- 2026/puzzle6/puzzle6.py
+++ 2026/puzzle6/puzzle6.py
@@ -1,94 +1,102 @@
 #!/usr/bin/python3
 from sympy import isprime
 
-def clone(grid):
-    cl = [row[:] for row in grid]
-    return cl
+class Machine:
+    def __init__(self, grid):
+        self.grid = grid
+        self.height = len(grid)
+        self.width = len(grid[0])
+        self.find_start()
+        self.outputs = {}
+        self.digit_grid = []
 
-def find_start(grid):
-    for y in range(0, height):
-        for x in range(0, width):
-            if grid[y][x] == 'S':
-                return x, y
+    def reset_digit_grid(self):
+        self.digit_grid = []
+        for i in range(0, self.height):
+            self.digit_grid.append([""] * self.width)
 
-def in_bounds(x, y):
-    return 0 <= x < width and 0 <= y < height
+    def find_start(self):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if grid[y][x] == 'S':
+                    self.start = (x, y)
 
-def rotate(grid, x, y, direction, visited, outputs):
-    pos = (x, y)
-    if pos in visited:
-        return
-    visited.add(pos)
+    def in_bounds(self, x, y):
+        return 0 <= x < self.width and 0 <= y < self.height
 
-    opposite = "L" if direction == "R" else "R"
+    def rotate(self, x, y, direction, visited):
+        pos = (x, y)
+        if pos in visited:
+            return
+        visited.add(pos)
 
-    for dir in dirs:
-        nx = x + dir[0]
-        ny = y + dir[1]
-        if in_bounds(nx, ny):
-            val = grid[ny][nx]
-            if val == "#" or val == "3":
-                rotate(grid, nx, ny, opposite, visited, outputs)
-            elif val == "*" and direction == "L":
-                grid[ny][nx] = "0"
-            elif (val == "*" or val == "0") and direction == "R":
-                grid[ny][nx] = "1"
-            elif len(outputs) and 'a' <= val <= 'z':
-                output = val.upper()
-                if output in outputs:
-                    ox, oy = outputs[output]
-                    rotate(grid, ox, oy, direction, visited, outputs)
+        opposite = "L" if direction == "R" else "R"
 
-def get_num(grid):
-    digits = []
-    for y in range(0, height):
-        for x in range(0, width):
-            if grid[y][x] == "0" or grid[y][x] == "1":
-                digits.append(grid[y][x])
+        for dir in dirs:
+            nx = x + dir[0]
+            ny = y + dir[1]
+            if self.in_bounds(nx, ny):
+                val = self.grid[ny][nx]
+                if val == "#" or val == "3":
+                    self.rotate(nx, ny, opposite, visited)
+                elif val == "*" and direction == "L":
+                    self.digit_grid[ny][nx] = "0"
+                elif (val == "*" or self.digit_grid[ny][nx] == "0") and direction == "R":
+                    self.digit_grid[ny][nx] = "1"
+                elif len(self.outputs) and 'a' <= val <= 'z':
+                    output = val.upper()
+                    if output in self.outputs:
+                        ox, oy = self.outputs[output]
+                        self.rotate(ox, oy, direction, visited)
 
-    return int("".join(digits), 2)
+    def get_num(self):
+        digits = []
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if self.digit_grid[y][x] == "0" or self.digit_grid[y][x] == "1":
+                    digits.append(self.digit_grid[y][x])
 
-def find_outputs(grid):
-    outputs = {}
-    for y in range(0, height):
-        for x in range(0, width):
-            if 'A' <= grid[y][x] <= 'Z':
-                outputs[grid[y][x]] = (x, y)
-    return outputs
+        return int("".join(digits), 2)
 
-def remove_prime_outputs(outputs):
-    to_delete = []
+    def find_outputs(self):
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                if 'A' <= self.grid[y][x] <= 'Z':
+                    self.outputs[self.grid[y][x]] = (x, y)
 
-    for output, location in outputs.items():
-        queue = [location]
-        visited = set()
-        visited.add(location)
+    def remove_prime_outputs(self):
+        to_delete = []
 
-        while(len(queue) > 0):
-            loc = queue.pop(0)
-            x, y = loc
+        for output, location in self.outputs.items():
+            queue = [location]
+            visited = set()
+            visited.add(location)
 
-            for dir in dirs:
-                nx = x + dir[0]
-                ny = y + dir[1]
-                if in_bounds(nx, ny) and grid[ny][nx] == "3":
-                    new_loc = (nx, ny)
-                    if new_loc in visited:
-                        continue
-                    queue.append(new_loc)
-                    visited.add(new_loc)
+            while(len(queue) > 0):
+                loc = queue.pop(0)
+                x, y = loc
 
-        group = len(visited) - 1
-        if isprime(group):
-            to_delete.append(output)
+                for dir in dirs:
+                    nx = x + dir[0]
+                    ny = y + dir[1]
+                    if self.in_bounds(nx, ny) and self.grid[ny][nx] == "3":
+                        new_loc = (nx, ny)
+                        if new_loc in visited:
+                            continue
+                        queue.append(new_loc)
+                        visited.add(new_loc)
 
-    for name in to_delete:
-        del(outputs[name])
+            group = len(visited) - 1
+            if isprime(group):
+                to_delete.append(output)
 
-def find_number(grid, start_x, start_y, outputs):
-    cl = clone(grid)
-    rotate(cl, start_x, start_y, "L", set(), outputs)
-    return get_num(cl)
+        for name in to_delete:
+            del(self.outputs[name])
+
+    def find_number(self):
+        self.reset_digit_grid()
+        self.rotate(self.start[0], self.start[1], "L", set())
+        return self.get_num()
 
 
 dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
@@ -96,14 +104,13 @@ dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 grid = []
 for line in open('input.txt').read().splitlines():
     grid.append(list(line))
-height = len(grid)
-width = len(grid[0])
-start_x, start_y = find_start(grid)
-outputs = find_outputs(grid)
 
-part1 = find_number(grid, start_x, start_y, {})
-part2 = find_number(grid, start_x, start_y, outputs)
-remove_prime_outputs(outputs)
-part3 = find_number(grid, start_x, start_y, outputs)
+machine = Machine(grid)
+
+part1 = machine.find_number()
+machine.find_outputs()
+part2 = machine.find_number()
+machine.remove_prime_outputs()
+part3 = machine.find_number()
 
 print(f"{part1}\n{part2}\n{part3}")
