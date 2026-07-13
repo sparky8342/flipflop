b8d1828938215d1f3d044edb73f338f03dd8a57e
David Bell <david.bell@gmail.com>
2026-07-13 06:07:04 +0100

    2026 puzzle 8 part 2

--- 2026/puzzle8/puzzle8.py
+++ 2026/puzzle8/puzzle8.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from collections import defaultdict
 
+lines = open('input.txt').read().splitlines()
+
 rules = {}
-for line in open('input.txt').read().splitlines():
+for line in lines:
     rule = line.split(" ")
     group = rule[0]
     if group not in rules:
@@ -18,3 +20,22 @@ for _ in range(0, 7):
     stoats = new_stoats
 
 print(sum(stoats.values()))
+
+rules = {}
+for line in lines:
+    rule = line.split(" ")
+    rules[rule[0] + rule[1]] = rule[2:]
+    rules[rule[1] + rule[0]] = rule[2:]
+
+stoats = ["A", "B"]
+
+for _ in range(0, 7):
+    new_stoats = [stoats[0]]
+    for i in range(0, len(stoats) - 1):
+        pair = stoats[i] + stoats[i+1]
+        if pair in rules:
+            new_stoats.extend(rules[pair])
+            new_stoats.append(stoats[i+1])
+    stoats = new_stoats
+
+print(len(stoats))
