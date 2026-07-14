0e28e9ddefb94c260eeec9fea7264d8325029d02
David Bell[C <david.bell@gmail.com>
2026-07-14 14:53:18 +0100

    2026 puzzle 9 part 2

--- 2026/puzzle9/puzzle9.py
+++ 2026/puzzle9/puzzle9.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from collections import deque
+import heapq
 
 def find_start_end(grid):
     for y in range(0, size):
@@ -41,3 +42,41 @@ while(len(queue) > 0):
             visited.add(new_loc)
 
 print(part1)
+
+part2 = 0
+queue = [(0, start[0], start[1])]
+visited = {}
+visited[start] = 0
+
+while(len(queue) > 0):
+    entry = heapq.heappop(queue)
+
+    distance, x, y = entry[0], entry[1], entry[2]
+
+    if x == end[0] and y == end[1]:
+        part2 = distance
+        break
+
+    new_distance = distance + 1
+    for dir in dirs:
+        nx = x + dir[0]
+        ny = y + dir[1]
+        if grid[ny][nx] != "#":
+            new_loc = (nx, ny)
+            if new_loc not in visited or visited[new_loc] > new_distance:
+                heapq.heappush(queue, (new_distance, nx, ny))
+                visited[new_loc] = new_distance
+
+            if grid[ny + dir[1]][nx + dir[0]] == "#":
+                continue
+
+            while grid[ny + dir[1]][nx + dir[0]] != "#":
+                nx += dir[0]
+                ny += dir[1]
+
+            new_loc = (nx, ny)
+            if new_loc not in visited or visited[new_loc] > new_distance:
+                heapq.heappush(queue, (new_distance, nx, ny))
+                visited[new_loc] = new_distance
+
+print(part2)
